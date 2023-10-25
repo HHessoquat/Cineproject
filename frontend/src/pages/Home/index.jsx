3d480738adf48b3540bf34ef01e9d1e75331a622
jeremy Dequeant <jeremy.dequeant@3wa.io>
2023-10-25 19:07:06 +0000

    add All sliders on Home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -39,13 +39,42 @@ function Home() {
     }, []);
     
     
-    console.log(movies.regularSessions)
+    console.log(movies)
     
     return (
         <main>
         
         {Object.keys(movies).length > 0 && <EventsSlider movies={movies} />}
-        {Object.keys(movies).length > 0 && movies.regularSessions && <MovieSlider movieRow={movies.regularSessions} />}
+        {Object.keys(movies).length > 0 && movies.regularSessions && (
+            <>
+                <h2>A l'affiche</h2>
+                <MovieSlider movieRow={movies.regularSessions} />
+            </>
+        )}
+        {Object.keys(movies).length > 0 && movies.movieToCome && (
+            <>
+                <h2>A venir</h2>
+                <MovieSlider movieRow={movies.movieToCome} />
+            </>
+        )}
+        {Object.keys(movies).length > 0 && movies.premiereSessions && (
+            <>
+                <h2>Les avants-premières</h2>
+                <MovieSlider movieRow={movies.premiereSessions} />
+            </>
+        )}
+        {Object.keys(movies).length > 0 && movies.wednesdaySessions && (
+            <>
+                <h2>Le mercredi des enfants</h2>
+                <MovieSlider movieRow={movies.wednesdaySessions} />
+            </>
+        )}
+        {Object.keys(movies).length > 0 && movies.friday && (
+            <>
+                <h2>Les vendredi cultes</h2>
+                <MovieSlider movieRow={movies.friday} />
+            </>
+        )}
         </main>
         
         )
